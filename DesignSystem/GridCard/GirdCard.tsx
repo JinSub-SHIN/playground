03cd6703f7236d15aa2bd371d337f60c146254b9
신 진섭 <hello2323@naver.com>
2023-06-01 11:36:59 +0900

    GridCard with DropDown Menu

--- DesignSystem/GridCard/GirdCard.tsx
+++ DesignSystem/GridCard/GirdCard.tsx
@@ -1,7 +1,16 @@
-import { Card, Col, Row } from 'antd'
-import { ReactNode } from 'react'
+import { Card, Col, Dropdown, MenuProps, Row } from 'antd'
+import { ReactNode, useState } from 'react'
 import { CardDescription } from './CardDescription'
 import styled from 'styled-components'
+import {
+	CardOptionIcon,
+	DeleteIcon,
+	DownLoadIcon,
+	EditIcon,
+	StarIcon,
+} from 'images'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'redux/reducer/rootReducer'
 
 const { Meta } = Card
 
@@ -28,6 +37,7 @@ export interface VideoDescription {
 	department: string
 	time: string
 	hashtag: string[]
+	star: boolean
 	children: ReactNode
 }
 
@@ -37,6 +47,7 @@ export interface PhotoDescription {
 	department: string
 	time: string
 	hashtag: string[]
+	star: boolean
 	children: ReactNode
 }
 
@@ -45,23 +56,90 @@ export interface MeMoDescription {
 	writer: string
 	content: string
 	time: string
+	star: boolean
 	children: ReactNode
 }
 
 export interface DirectCameraDescription {
 	type: 'direct'
+	jobName: string
 	department: string
 	conductor: string
 	workTime: string
+	dataUrl: string
 	children: ReactNode
+	star?: boolean
 }
+
+/**
+ * store 전용 데이터
+ */
+export interface VideoDescriptionStoreOnly {
+	type: 'video'
+	joinPerson: string[]
+	department: string
+	time: string
+	hashtag: string[]
+	star: boolean
+}
+
+/**
+ * store 전용 데이터
+ */
+export interface PhotoDescriptionStoreOnly {
+	type: 'photo'
+	joinPerson: string[]
+	department: string
+	time: string
+	hashtag: string[]
+	star: boolean
+}
+
+/**
+ * store 전용 데이터
+ */
+export interface MeMoDescriptionStoreOnly {
+	type: 'memo'
+	writer: string
+	content: string
+	time: string
+	star: boolean
+}
+
+/**
+ * store 전용 데이터
+ */
+export interface DirectCameraDescriptionStoreOnly {
+	type: 'direct'
+	jobName: string
+	department: string
+	conductor: string
+	workTime: string
+	dataUrl: string
+}
+
+const FlexTitle = styled.div`
+	display: flex;
+	justify-content: space-between;
+`
+
+const ButtonTitle = styled.div`
+	display: flex;
+	justify-content: space-around;
+`
+
+const TitleSpan = styled.span``
+
+const CardTitleImg = styled.img``
+
+const IconStyled = styled.img``
 
 /**
  *
  * @paramType GirdCardProps
  * @param clickFunction (index: number) => void
  * @param title string
- * @param description VideoDescription[] Type || PhotoDescription[] Type
+ * @param description (PhotoDescription | VideoDescription | MeMoDescription | DirectCameraDescription)[]
  * @returns
  */
 export const GridCard = ({
@@ -69,21 +147,173 @@ export const GridCard = ({
 	title,
 	description,
 }: GirdCardProps) => {
+	const dispatch = useDispatch()
+	const cardItems = useSelector((state: RootState) => state.cardList.items)
+	const [cardIndex, setCardIndex] = useState<number>()
+
+	const handleShowOption = (e: React.MouseEvent) => {
+		e.stopPropagation()
+	}
+
+	const handleMenuClick: MenuProps['onClick'] = e => {
+		e.domEvent.stopPropagation()
+
+		console.log('click', e)
+		console.log(cardItems[cardIndex!])
+
+		if (e.key == '1') {
+			console.log('다운로드 작업')
+		}
+
+		if (e.key == '2') {
+			console.log('수정 작업')
+		}
+
+		if (e.key == '3') {
+			console.log('즐겨찾기 작업')
+		}
+
+		if (e.key == '4') {
+			console.log('삭제작업')
+		}
+	}
+
+	const returnItems = (status: boolean) => {
+		let items: MenuProps['items'] = []
+
+		if (!status) {
+			items = [
+				{
+					key: '1',
+					label: '다운로드',
+					icon: <IconStyled src={DownLoadIcon} />,
+				},
+				{
+					key: '2',
+					label: '수정',
+					icon: <IconStyled src={EditIcon} />,
+				},
+				{
+					key: '3',
+					label: '즐겨찾기',
+					icon: <IconStyled src={StarIcon} />,
+				},
+				{
+					key: '4',
+					label: '삭제',
+					icon: <IconStyled src={DeleteIcon} />,
+				},
+			]
+		} else {
+			items = [
+				{
+					key: '1',
+					label: '다운로드',
+					icon: <IconStyled src={DownLoadIcon} />,
+				},
+				{
+					key: '2',
+					label: '수정',
+					icon: <IconStyled src={EditIcon} />,
+				},
+				{
+					key: '3',
+					label: '즐겨찾기 해제',
+					icon: <IconStyled src={StarIcon} />,
+				},
+				{
+					key: '4',
+					label: '삭제',
+					icon: <IconStyled src={DeleteIcon} />,
+				},
+			]
+		}
+
+		const menuProps = {
+			items,
+			onClick: handleMenuClick,
+		}
+
+		return menuProps
+	}
+
+	const handleOpen = (index: number) => {
+		setCardIndex(index)
+	}
+
 	return (
 		<>
 			<Row gutter={[16, 16]}>
 				{description.map((item, index) => (
 					<Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 6 }} key={index}>
-						<Card
-							hoverable
-							cover={item.children}
-							onClick={() => clickFunction(index)}
-						>
-							<Meta
-								title={title}
-								description={<CardDescription description={item} />}
-							/>
-						</Card>
+						{item.type == 'direct' ? (
+							<Card
+								hoverable
+								cover={item.children}
+								onClick={() => clickFunction(index)}
+							>
+								<Meta
+									title={
+										<FlexTitle>
+											<TitleSpan>{title}</TitleSpan>
+										</FlexTitle>
+									}
+									description={<CardDescription description={item} />}
+								/>
+							</Card>
+						) : (
+							<Card
+								hoverable
+								cover={item.children}
+								onClick={() => clickFunction(index)}
+							>
+								{item.star ? (
+									<>
+										<Meta
+											title={
+												<FlexTitle>
+													<TitleSpan>{title}</TitleSpan>
+													<Dropdown
+														menu={returnItems(true)}
+														placement="bottom"
+														trigger={['click', 'hover']}
+														onOpenChange={() => handleOpen(index)}
+													>
+														<CardTitleImg
+															src={CardOptionIcon}
+															onClick={handleShowOption}
+														/>
+													</Dropdown>
+												</FlexTitle>
+											}
+											description={<CardDescription description={item} />}
+										/>
+									</>
+								) : (
+									<>
+										<Meta
+											title={
+												<FlexTitle>
+													<TitleSpan>{title}</TitleSpan>
+													<Dropdown
+														menu={returnItems(false)}
+														placement="bottom"
+														trigger={['click', 'hover']}
+														onOpenChange={() => handleOpen(index)}
+													>
+														<CardTitleImg
+															src={CardOptionIcon}
+															onClick={handleShowOption}
+														/>
+													</Dropdown>
+												</FlexTitle>
+											}
+											description={<CardDescription description={item} />}
+										/>
+									</>
+								)}
+							</Card>
+						)}
 					</Col>
 				))}
 			</Row>
